6a799517560b89db88e8e8dfd1cfa18b75c527d0
antonioGitea <antsanlea@alu.edu.gva.es>
2026-02-23 21:02:58 +0100

    Seeders y Factories lanzados
    Modelos corregidos para que puedan trabajar con seeders y factories. Importaciones agregadas y timstamps retocadas en los modelos donde no se usan

--- app/Models/Especie.php
+++ app/Models/Especie.php
@@ -1,8 +1,12 @@
 <?php
     namespace App\Models;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
 
     class Especie extends Model {
+        use HasFactory;
+        public $timestamps = false;
+        
         protected $table = "especies";
         protected $fillable = 
                             [   'nombre_cientifico',
--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Evento extends Model
 {
+    use HasFactory;
+    public $timestamps = false;
+    
     protected $table = "eventos";
     protected $fillable = ['nombre', 'descripcion', 'ubicacion', 'fecha', 'tipo_terreno', 'tipo_evento', 'imagen'];
     
